e4b40c41a349d85d08d2779e02ebb2fa6f802b04
Yousef <yousefsameh24@gmail.com>
2023-11-22 16:55:20 +0200

    Refactor GET function to include pagination

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,8 +2,11 @@ import { NextResponse, NextRequest } from 'next/server'
 import dbConnect from '@/utils/dbConnect'
 import Order from '@/models/order'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   await dbConnect()
-  const orders = await Order.find({}).sort({ createdAt: -1 })
+  const searchParams = req.nextUrl.searchParams
+  const page = parseInt(searchParams.get('page') || '1')
+  const limit = parseInt(searchParams.get('limit') || '10')
+  const orders = await Order.find({}).sort({ createdAt: -1 }).skip((page - 1) * limit).limit(limit)
   return NextResponse.json(orders)
 }
