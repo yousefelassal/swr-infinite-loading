8f7afe12939868fe2407bc14a160beaa12faa285
Yousef <yousefsameh24@gmail.com>
2023-11-22 19:03:12 +0200

    Add Loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,23 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col gap-3 w-full p-4">
+    <div className="flex justify-between">
+      <div className=" animate-pulse bg-gray-600 h-6 w-16 rounded-lg" />
+      <div className=" animate-pulse bg-gray-600 h-6 w-16 rounded-lg" />
+    </div>
+    <div className="flex flex-col gap-4">
+      <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+      <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+      <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+      <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+      <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+    </div>
+    
+    <div className="flex gap-2">
+        <div className="rounded-xl flex-1 h-10 animate-pulse bg-gray-600" />
+        <div className="rounded-xl flex-1 h-10 animate-pulse bg-gray-600" />
+        <div className="rounded-xl w-20 h-10 animate-pulse bg-gray-600" />
+    </div>
+  </div>
+  )
+}
