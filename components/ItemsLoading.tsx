d2a93791de0ab7f0624788da76bddf60382dd81f
Yousef <yousefsameh24@gmail.com>
2023-11-24 00:45:58 +0200

    Add ItemsLoading component

---
+++ components/ItemsLoading.tsx
@@ -0,0 +1,9 @@
+export default function ItemsLoading() {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+        <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+      </div>
+    )
+  }
+  
