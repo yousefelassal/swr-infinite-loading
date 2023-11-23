3ed090de2fc3f043d95b7fd1e2215b87f0373f9a
Yousef <yousefsameh24@gmail.com>
2023-11-23 20:14:22 +0200

    Add navigation tabs to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans';
 import './globals.css'
+import Nav from '@/components/Nav'
 
 export const metadata: Metadata = {
   title: 'SWR Infinite Loading',
   description: 'Fullstack data fetching app using MondoDB, Next',
 }
+
+const tabs = [
+  { href: '/', label: 'Mongo' },
+  { href: '/postgres', label: 'Postgres' },
+]
 
 export default function RootLayout({
   children,
@@ -15,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={GeistSans.className}>
+        <Nav tabs={tabs} />
         {children}
       </body>
     </html>
