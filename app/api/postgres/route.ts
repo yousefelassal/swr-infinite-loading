a44a50d0ed76712721fa63e80949de336ebc06dc
Yousef <yousefsameh24@gmail.com>
2023-11-23 19:13:42 +0200

    Create Postgres API

---
+++ app/api/postgres/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from 'next/server'
+import { sql } from '@vercel/postgres'
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams
+    const page = parseInt(searchParams.get('page') || '1')
+    const limit = parseInt(searchParams.get('limit') || '10')
+    const orders = await sql`SELECT * FROM orders ORDER BY created_at DESC OFFSET ${page - 1} LIMIT ${limit}`
+    return NextResponse.json(orders)
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    const newOrder = {
+        name: body.name,
+        value: body.value,
+    }
+    const order = await sql`INSERT INTO orders (name, value) VALUES (${newOrder.name}, ${newOrder.value}) RETURNING *`
+    return NextResponse.json({ message: 'Order created successfully', order })
+}
