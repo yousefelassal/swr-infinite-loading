10fbabeb1d3814179081044bdb02ab90fc6a5bac
Yousef <yousefsameh24@gmail.com>
2023-11-23 20:13:24 +0200

    Update font in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { GeistSans } from 'geist/font/sans';
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'SWR Infinite Loading',
@@ -16,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.className}>
+        {children}
+      </body>
     </html>
   )
 }
