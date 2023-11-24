60e94f2db5acc865c61813dd468dee059c17b9c0
Yousef <yousefsameh24@gmail.com>
2023-11-24 15:02:01 +0200

    Refactor pagination logic in GET route

--- app/api/postgres/route.ts
+++ app/api/postgres/route.ts
@@ -6,12 +6,12 @@ export async function GET(req: NextRequest) {
     const page = parseInt(searchParams.get('page') || '1')
     const limit = parseInt(searchParams.get('limit') || '10')
     const q = searchParams.get('q') || ''
+    const offset = (page - 1) * limit
     if (q === '') {
-        const orders = await sql`SELECT * FROM orders ORDER BY created_at DESC OFFSET ${page - 1} LIMIT ${limit}`
+        const orders = await sql`SELECT * FROM orders ORDER BY created_at DESC OFFSET ${offset} LIMIT ${limit}`
         return NextResponse.json(orders.rows)
     }
-    const query = `%${q}%`
-    const orders = await sql`SELECT * FROM orders WHERE name ILIKE ${query} ORDER BY created_at DESC OFFSET ${page - 1} LIMIT ${limit}`
+    const orders = await sql`SELECT * FROM orders WHERE name ILIKE ${q} ORDER BY created_at DESC OFFSET ${offset} LIMIT ${limit}`
     return NextResponse.json(orders.rows)
 }
 
