5b4fe6e0f70562634acbd8dc784c2cc1bde41a84
Yousef <yousefsameh24@gmail.com>
2023-11-24 22:49:37 +0200

    Update ItemsLoading height

--- components/ItemsLoading.tsx
+++ components/ItemsLoading.tsx
@@ -1,8 +1,8 @@
 export default function ItemsLoading() {
     return (
       <div className="flex flex-col gap-4">
-        <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
-        <div className="rounded-xl animate-pulse bg-gray-600 h-14 w-full" />
+        <div className="rounded-xl animate-pulse bg-gray-600 h-[73.33px] w-full" />
+        <div className="rounded-xl animate-pulse bg-gray-600 h-[73.33px] w-full" />
       </div>
     )
   }
