d2b5728c2ad5651369c83ace93452976ef47f68c
Yousef <yousefsameh24@gmail.com>
2023-11-25 15:32:33 +0200

    Add PostgreSQL server actions

---
+++ app/postgres/actions.ts
@@ -0,0 +1,64 @@
+'use server'
+
+import { sql } from '@vercel/postgres';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache'
+
+export async function createOrder(prevState: any, formData: FormData) {
+  const schema = z.object({
+    name: z.string().min(1),
+    value: z.string().min(1),
+  })
+  const parse = schema.safeParse({
+    name: formData.get('name'),
+    value: formData.get('value'),
+  })
+
+  if (!parse.success) {
+    return { message: 'Failed to create order' }
+  }
+
+  const data = parse.data
+
+  try {
+    await sql`
+      INSERT INTO orders (name, value)
+      VALUES (${data.name}, ${data.value})
+      RETURNING *
+    `;
+    revalidatePath('/')
+    
+    return { message: `Added name ${data.name}` }
+  } catch (e) {
+    return { message: 'Failed to create name' }
+  }
+}
+
+export async function create(name:string, value:any) {
+    await sql`
+      INSERT INTO orders (name, value)
+      VALUES (${name}, ${value})
+      RETURNING *
+    `;
+}
+
+export async function read(id:number) {
+    const orders = await sql`
+      SELECT * FROM orders WHERE id = ${id}
+    `;
+    return orders.rows;
+}
+
+export async function update(order:any) {
+    await sql`
+      UPDATE orders
+      SET name = ${order.name}, value = ${order.value}
+      WHERE id = ${order.id}
+    `;
+}
+
+export async function del(id:number) {
+    await sql`
+      DELETE FROM orders WHERE id = ${id}
+    `;
+}
