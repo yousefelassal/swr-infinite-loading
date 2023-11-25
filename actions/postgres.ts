010af95cc8c488c5088b8ec821496b0d98274fea
Yousef <yousefsameh24@gmail.com>
2023-11-25 15:29:50 +0200

    Add createOrder function to handle order creation

--- actions/postgres.ts
+++ actions/postgres.ts
@@ -1,6 +1,38 @@
 'use server'
 
 import { sql } from '@vercel/postgres';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache'
+
+export async function createOrder(prevState: any, formData: FormData) {
+  const schema = z.object({
+    name: z.string().min(1),
+    value: z.string().min(1),
+  })
+  const parse = schema.safeParse({
+    name: formData.get('name'),
+    value: formData.get('value'),
+  })
+
+  if (!parse.success) {
+    return { message: 'Failed to create order' }
+  }
+
+  const data = parse.data
+
+  try {
+    await sql`
+      INSERT INTO orders (name, value)
+      VALUES (${data.name}, ${data.value})
+      RETURNING *
+    `;
+    revalidatePath('/')
+    
+    return { message: `Added name ${data.name}` }
+  } catch (e) {
+    return { message: 'Failed to create name' }
+  }
+}
 
 export async function create(name:string, value:any) {
     'use server'
