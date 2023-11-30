421e300ccce67a9c34034748d6f9ba128dae02b8
Yousef <yousefsameh24@gmail.com>
2023-11-30 13:01:52 +0200

    Update next.config.js to include MDX files and add markdown plugins

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-const withMDX = require('@next/mdx')()
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 }
  
-module.exports = withMDX(nextConfig)
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
