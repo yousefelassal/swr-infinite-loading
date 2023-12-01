f155a4fff7b319988b446fd9eaffca966b148074
Yousef <yousefsameh24@gmail.com>
2023-12-01 15:49:29 +0200

    Update Nav component styling

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,7 +13,7 @@ export default function Nav({ tabs }: { tabs: Tab[] }) {
   const pathname = usePathname()
 
   return (
-    <div className="flex items-center p-2 justify-center w-screen">
+    <div className="flex items-center p-2 justify-center w-full">
         <div className="flex p-1 border border-violet-900/20 bg-violet-950/20 rounded-2xl flex-row gap-2 items-center justify-center min-w-[300px] w-1/2">
             {tabs.map((tab) => (
                 <Link key={tab.label} href={tab.href} className={`flex rounded-xl flex-1 flex-col gap-1 relative items-center sm:text-lg justify-center p-4 ${pathname === tab.href ? 'text-white' : 'text-gray-300 hover:text-gray-100'} cursor-pointer transition-colors`}>
