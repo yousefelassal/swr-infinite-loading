d2937395379dbf06aa92c027739b1288aab17ff5
Yousef <yousefsameh24@gmail.com>
2023-12-10 18:31:55 +0200

    Add Code component and set theme to 'github-dark'

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,11 @@
 import type { MDXComponents } from 'mdx/types'
+import { Code } from 'bright'
+
+Code.theme = 'github-dark'
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    pre: Code
   }
 }
