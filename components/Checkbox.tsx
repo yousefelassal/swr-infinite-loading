8a8d53c071905ee7376e28e35796d0ad90bf722d
Yousef <yousefsameh24@gmail.com>
2023-12-11 11:11:51 +0200

    Add tooltip to Checkbox component

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 import { BookmarkIcon as BookmarkOutline, ArrowPathIcon } from '@heroicons/react/24/outline'
 import { BookmarkIcon } from '@heroicons/react/24/solid'
 import { patch } from '@/services/mongo'
-import { updateSaved } from '@/app/(home)/postgres/actions'
+import { updateSaved } from '@/services/postgres'
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export default function Checkbox({ order, mutate, db }:any) {
   const [loading, setLoading] = useState(false)
@@ -20,6 +27,9 @@ export default function Checkbox({ order, mutate, db }:any) {
     }
   
   return (
+    <TooltipProvider>
+    <Tooltip>
+    <TooltipTrigger asChild>
     <label htmlFor={`${order.id}`} className="block py-2 px-4 rounded-md hover:bg-violet-400/20 relative cursor-pointer select-none">
         <input 
             value={order.saved}
@@ -43,5 +53,11 @@ export default function Checkbox({ order, mutate, db }:any) {
         : <BookmarkOutline className="h-5 w-5 text-violet-400 hover:scale-110 relative top-0 left-0 transition-all duration-300" />
         }
     </label>
+    </TooltipTrigger>
+    <TooltipContent>
+        { order.saved ? "Remove from saved" : "Save" }
+    </TooltipContent>
+    </Tooltip>
+    </TooltipProvider>
   )
 }
