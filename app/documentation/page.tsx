e2953732c0f1e36c9f48ec24079d470a44ba45a0
Yousef <yousefsameh24@gmail.com>
2023-12-11 09:20:04 +0200

    Remove unused CodePostgres component

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -1,11 +1,9 @@
 import Code from '@/components/Code.mdx'
-import CodePostgres from '@/components/CodePostgres.mdx'
 
 export default function page() {
   return (
     <div className="flex p-4 flex-col gap-4">
       <Code />
-      <CodePostgres />
     </div>
   )
 }
