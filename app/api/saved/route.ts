3cb801516e0979e57827a7a0f2bac15339af8800
Yousef <yousefsameh24@gmail.com>
2023-12-12 15:26:30 +0200

    Refactor limit calculation in saved route

--- app/api/saved/route.ts
+++ app/api/saved/route.ts
@@ -6,14 +6,14 @@ import { sql } from '@vercel/postgres'
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams
     const page = parseInt(searchParams.get('page') || '1')
-    const limit = parseInt(searchParams.get('limit') || '10')
+    const limit = Math.floor(parseInt(searchParams.get('limit') || '10') / 2)
     const q = searchParams.get('q') || ''
     const query = `%${q}%`
     const offset = (page - 1) * limit
     
     let allSaved:any = []
 
-    const postgresSaved = await sql`SELECT * FROM orders WHERE name ILIKE ${query} AND saved = true ORDER BY "savedAt" DESC OFFSET ${offset} LIMIT ${limit/2}`
+    const postgresSaved = await sql`SELECT * FROM orders WHERE name ILIKE ${query} AND saved = true ORDER BY "savedAt" DESC OFFSET ${offset} LIMIT ${limit}`
     const postgres = postgresSaved.rows.map((row:any) => {
         return {
             ...row,
@@ -23,10 +23,12 @@ export async function GET(req: NextRequest) {
     allSaved = allSaved.concat(postgres)
 
     await dbConnect()
-    const mongoSaved = await Order.find({ name: { $regex: q, $options: 'i' }, saved:true }).sort({ savedAt: -1 }).skip((page - 1) * limit).limit(limit/2)
+    const mongoSaved = await Order.find({ name: { $regex: q, $options: 'i' }, saved:true }).sort({ savedAt: -1 }).skip((page - 1) * limit).limit(limit)
     const mongo = mongoSaved.map((row:any) => {
+        const { _id, __v, ...rest } = row._doc
         return {
-            ...row._doc,
+            ...rest,
+            id: row._doc._id,
             db: 'mongo'
         }
     })
