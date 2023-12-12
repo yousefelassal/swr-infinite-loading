06af46eb5a8bc0d7c6c4e7982a86b5b8f1377941
Yousef <yousefsameh24@gmail.com>
2023-12-12 15:49:05 +0200

    Refactor SavedLoading component

--- components/SavedLoading.tsx
+++ components/SavedLoading.tsx
@@ -1,8 +1,8 @@
 export default function SavedLoading() {
   return (
-    <div className="flex flex-col gap-4">
+    <>
       <div className="rounded-xl animate-pulse bg-gray-600 h-[194px] w-full" />
       <div className="rounded-xl animate-pulse bg-gray-600 h-[194px] w-full" />
-    </div>
+    </>
   )
 }
