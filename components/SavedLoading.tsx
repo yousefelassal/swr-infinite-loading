dc47b14081172628e0fd862b8b88155f0e467dd1
Yousef <yousefsameh24@gmail.com>
2023-12-12 15:33:41 +0200

    Create Saved Loading Component

---
+++ components/SavedLoading.tsx
@@ -0,0 +1,8 @@
+export default function SavedLoading() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="rounded-xl animate-pulse bg-gray-600 h-[73.33px] w-full" />
+      <div className="rounded-xl animate-pulse bg-gray-600 h-[73.33px] w-full" />
+    </div>
+  )
+}
