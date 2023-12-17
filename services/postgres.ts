27222cacb1201e79f39d3073a2efd40ff41f4232
Yousef Sameh <76617202+yousefelassal@users.noreply.github.com>
2023-12-17 12:32:38 +0000

    Add update function to postgres service

--- services/postgres.ts
+++ services/postgres.ts
@@ -6,6 +6,10 @@ export const updateSaved = (order:any) => {
     return axios.patch(`${baseURL}/${order.id}`, order);
 }
 
+export const update = (order:any) => {
+    return axios.put(`${baseURL}/${order.id}`, order);
+}
+
 export const del = (id:number) => {
     return axios.delete(`${baseURL}/${id}`);
 }
