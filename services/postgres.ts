1dd8a1ef53eec833e108667d2c4856da49201df5
Yousef Sameh <76617202+yousefelassal@users.noreply.github.com>
2023-12-17 11:15:53 +0000

    Add delete function to Postgres service

--- services/postgres.ts
+++ services/postgres.ts
@@ -5,3 +5,7 @@ const baseURL = '/api/postgres';
 export const updateSaved = (order:any) => {
     return axios.patch(`${baseURL}/${order.id}`, order);
 }
+
+export const del = (id:number) => {
+    return axios.delete(`${baseURL}/${id}`);
+}
